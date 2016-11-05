6634764bc7320b20441c4dad9b43de0e1a9cf415
Marcelo Canina <me@marcanuy.com>
2016-11-05 15:49:47 -0300

    Added travis authentication to github

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -25,6 +25,18 @@ git --work-tree=_site commit -m "Published changes"
 git symbolic-ref HEAD refs/heads/master
 git reset
 
+# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+# https://gist.github.com/domenic/ec8b0fc8ab45f39403dd
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
+chmod 600 deploy_key
+eval `ssh-agent -s`
+ssh-add deploy_key
+
+
 # Push both branches to GitHub
 #git push --all origin
 git push origin gh-pages
