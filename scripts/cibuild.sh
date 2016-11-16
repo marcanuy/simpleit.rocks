b42f55fd5fce25a6772f5197b61684a5c2e1e2b4
Marcelo Canina <me@marcanuy.com>
2016-11-16 11:23:53 -0300

    Fix #4 bootrap 4.0.0-alpha.5 bootstrap alpha 4 breaks htmlproofer

--- scripts/cibuild.sh
+++ scripts/cibuild.sh
@@ -2,4 +2,6 @@
 set -ev # halt script on error
 
 bundle exec jekyll build
-bundle exec htmlproofer ./_site --disable-external
+# bundle exec htmlproofer ./_site --disable-external
+# temporary fix to use bootstrap 4.0.0-alpha.5
+bundle exec htmlproofer ./_site --disable-external --file-ignore "/.+\/bower_components\/tether.+/"
