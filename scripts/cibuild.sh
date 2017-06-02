1b46b6df6462df4e43a58aed81924c729db34526
Travis CI <me@marcanuy.com>
2017-06-02 22:25:02 +0000

    Deploy to GitHub Pages: 6bd9d307105e49e38c4d99a5138f4e6a7e47041f

--- scripts/cibuild.sh
+++ scripts/cibuild.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -ev # halt script on error
 
-bundle exec jekyll build
+JEKYLL_ENV=production bundle exec jekyll build
 # bundle exec htmlproofer ./_site --disable-external
 # temporary fix to use bootstrap 4.0.0-alpha.5
-bundle exec htmlproofer ./_site --disable-external --file-ignore "/.+\/bower_components\/tether.+/"
+JEKYLL_ENV=production bundle exec htmlproofer ./_site --disable-external --file-ignore "/.+\/bower_components\/tether.+/"
